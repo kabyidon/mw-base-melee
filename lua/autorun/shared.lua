0ca3c96032a1809637040a456b60467d3234543e
kabyidon <75186410+kabyidon@users.noreply.github.com>
2022-11-20 19:21:32 -0600

    refactor out local functions

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -1,133 +1,128 @@
 AddCSLuaFile()
 
+--VJ L4D2 SI M16 code
+function Flinch(wpn, v)
+    v.NextL4DFlinchT = v.NextL4DFlinchT or 0
+    if CurTime() < v.NextL4DFlinchT then return end
+    local isCI = v.Zombie_CanPuke != nil
+    v.Flinching = true
+    v:StopAttacks(true)
+    v.PlayingAttackAnimation = false
+    local posF = (v:GetPos() +v:OBBCenter()) +v:GetForward() *115
+    local posB = (v:GetPos() +v:OBBCenter()) +v:GetForward() *-115
+    local posL = (v:GetPos() +v:OBBCenter()) +v:GetRight() *-115
+    local posR = (v:GetPos() +v:OBBCenter()) +v:GetRight() *115
+    local tbl = {posF,posB,posL,posR}
+    local nearestDist = 9999999
+    local cPos = nil
+    for _,v in pairs(tbl) do
+        if v:Distance(wpn:GetPos()) < nearestDist then
+            nearestDist = v:Distance(wpn:GetPos())
+            cPos = v
+        end
+    end
+    if cPos == nil then return end
+    if isCI && !v.Shove_Forward then
+        v.Shove_Forward = "Shoved_Backward_01"
+        v.Shove_Backward = "Shoved_Forward_01"
+        v.Shove_Left = "Shoved_Rightward_01"
+        v.Shove_Right = "Shoved_Leftward_01"
+    end
+    local anim = (cPos == posF && v.Shove_Forward) or (cPos == posB && v.Shove_Backward) or (cPos == posL && v.Shove_Left) or (cPos == posR && v.Shove_Right) or v.Shove_Forward or false
+    if isCI then
+        if anim == v.Shove_Forward then
+            local tr = util.TraceHull({
+                start = (v:GetPos() +v:OBBCenter()),
+                endpos = posB,
+                mask = MASK_SHOT,
+                filter = {v},
+                mins = v:OBBMins(),
+                maxs = v:OBBMaxs()
+            })
+            if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
+                anim = "Shoved_Backward_IntoWall_01"
+            end
+        elseif anim == v.Shove_Backward then
+            local tr = util.TraceHull({
+                start = (v:GetPos() +v:OBBCenter()),
+                endpos = posF,
+                mask = MASK_SHOT,
+                filter = {v},
+                mins = v:OBBMins(),
+                maxs = v:OBBMaxs()
+            })
+            if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
+                anim = "Shoved_Forward_IntoWall_02"
+            end
+        elseif anim == v.Shove_Left then
+            local tr = util.TraceHull({
+                start = (v:GetPos() +v:OBBCenter()),
+                endpos = posR,
+                mask = MASK_SHOT,
+                filter = {v},
+                mins = v:OBBMins(),
+                maxs = v:OBBMaxs()
+            })
+            if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
+                anim = "Shoved_Rightward_IntoWall_01"
+            end
+        elseif anim == v.Shove_Right then
+            local tr = util.TraceHull({
+                start = (v:GetPos() +v:OBBCenter()),
+                endpos = posL,
+                mask = MASK_SHOT,
+                filter = {v},
+                mins = v:OBBMins(),
+                maxs = v:OBBMaxs()
+            })
+            if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
+                anim = "Shoved_Leftward_IntoWall_02"
+            end
+        end
+    end
+    if !anim then return end
+    if v.OnShoved then
+        anim = v:OnShoved(anim,wpn.Owner)
+    end
+    v:VJ_ACT_PLAYACTIVITY(anim,true,false,false)
+    local animdur = (v:DecideAnimationLength(anim,false))
+    timer.Create("timer_act_flinching" .. v:EntIndex(),animdur,1,function() v.Flinching = false end)
+    v.NextFlinchT = CurTime() +animdur
+    v.NextL4DFlinchT = CurTime() +(animdur *0.25)
+end
+
+-- Refactored code from weapon_vj_base_ex
+function GetNearestPoint(wpn, argent, SameZ)
+    if !IsValid(argent) then return end
+    SameZ = SameZ or false -- Should the Z of the pos be the same as the NPC's?
+    local myNearestPoint = wpn:GetOwner():EyePos()
+    local NearestPositions = {MyPosition=Vector(0,0,0), EnemyPosition=Vector(0,0,0)}
+    local Pos_Enemy, Pos_Self = argent:NearestPoint(myNearestPoint + argent:OBBCenter()), wpn:NearestPoint(argent:GetPos() + wpn:OBBCenter())
+    Pos_Enemy.z, Pos_Self.z = argent:GetPos().z, myNearestPoint.z
+    if SameZ == true then
+        Pos_Enemy = Vector(Pos_Enemy.x,Pos_Enemy.y,wpn:SetNearestPointToEntityPosition().z)
+        Pos_Self = Vector(Pos_Self.x,Pos_Self.y,wpn:SetNearestPointToEntityPosition().z)
+    end
+    NearestPositions.MyPosition = Pos_Self
+    NearestPositions.EnemyPosition = Pos_Enemy
+    return NearestPositions
+end
 
 hook.Add( "Initialize", "ReplaceMeleeBehaviourModuleForVJL4D2", function()
 
-    -- Inspired by Clementine's MW Base Plus  
+    -- Overwrite method (may conflict with MW base plus)
     local MW = weapons.GetStored( "mg_base" )
     local tick = engine.TickInterval()
 
-    -- local randomMaterial = Material("mg/random")
-    -- local clearMaterial = Material("mg/clear")
-
     function MW:MeleeBehaviourModule()
-
-        --VJ L4D2 SI M16 code
-        local function Flinch(v)
-            v.NextL4DFlinchT = v.NextL4DFlinchT or 0
-            if CurTime() < v.NextL4DFlinchT then return end
-            local isCI = v.Zombie_CanPuke != nil
-            v.Flinching = true
-            v:StopAttacks(true)
-            v.PlayingAttackAnimation = false
-            local posF = (v:GetPos() +v:OBBCenter()) +v:GetForward() *115
-            local posB = (v:GetPos() +v:OBBCenter()) +v:GetForward() *-115
-            local posL = (v:GetPos() +v:OBBCenter()) +v:GetRight() *-115
-            local posR = (v:GetPos() +v:OBBCenter()) +v:GetRight() *115
-            local tbl = {posF,posB,posL,posR}
-            local nearestDist = 9999999
-            local cPos = nil
-            for _,v in pairs(tbl) do
-                if v:Distance(self:GetPos()) < nearestDist then
-                    nearestDist = v:Distance(self:GetPos())
-                    cPos = v
-                end
-            end
-            if cPos == nil then return end
-            if isCI && !v.Shove_Forward then
-                v.Shove_Forward = "Shoved_Backward_01"
-                v.Shove_Backward = "Shoved_Forward_01"
-                v.Shove_Left = "Shoved_Rightward_01"
-                v.Shove_Right = "Shoved_Leftward_01"
-            end
-            local anim = (cPos == posF && v.Shove_Forward) or (cPos == posB && v.Shove_Backward) or (cPos == posL && v.Shove_Left) or (cPos == posR && v.Shove_Right) or v.Shove_Forward or false
-            if isCI then
-                if anim == v.Shove_Forward then
-                    local tr = util.TraceHull({
-                        start = (v:GetPos() +v:OBBCenter()),
-                        endpos = posB,
-                        mask = MASK_SHOT,
-                        filter = {v},
-                        mins = v:OBBMins(),
-                        maxs = v:OBBMaxs()
-                    })
-                    if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
-                        anim = "Shoved_Backward_IntoWall_01"
-                    end
-                elseif anim == v.Shove_Backward then
-                    local tr = util.TraceHull({
-                        start = (v:GetPos() +v:OBBCenter()),
-                        endpos = posF,
-                        mask = MASK_SHOT,
-                        filter = {v},
-                        mins = v:OBBMins(),
-                        maxs = v:OBBMaxs()
-                    })
-                    if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
-                        anim = "Shoved_Forward_IntoWall_02"
-                    end
-                elseif anim == v.Shove_Left then
-                    local tr = util.TraceHull({
-                        start = (v:GetPos() +v:OBBCenter()),
-                        endpos = posR,
-                        mask = MASK_SHOT,
-                        filter = {v},
-                        mins = v:OBBMins(),
-                        maxs = v:OBBMaxs()
-                    })
-                    if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
-                        anim = "Shoved_Rightward_IntoWall_01"
-                    end
-                elseif anim == v.Shove_Right then
-                    local tr = util.TraceHull({
-                        start = (v:GetPos() +v:OBBCenter()),
-                        endpos = posL,
-                        mask = MASK_SHOT,
-                        filter = {v},
-                        mins = v:OBBMins(),
-                        maxs = v:OBBMaxs()
-                    })
-                    if tr.Hit && tr.HitPos:Distance(v:GetPos() +v:OBBCenter()) <= 25 then
-                        anim = "Shoved_Leftward_IntoWall_02"
-                    end
-                end
-            end
-            if !anim then return end
-            if v.OnShoved then
-                anim = v:OnShoved(anim,self.Owner)
-            end
-            v:VJ_ACT_PLAYACTIVITY(anim,true,false,false)
-            local animdur = (v:DecideAnimationLength(anim,false))
-            timer.Create("timer_act_flinching" .. v:EntIndex(),animdur,1,function() v.Flinching = false end)
-            v.NextFlinchT = CurTime() +animdur
-            v.NextL4DFlinchT = CurTime() +(animdur *0.25)
-        end
-
-        -- Refactored code from weapon_vj_base_ex
-        local function GetNearestPoint(argent,SameZ)
-            if !IsValid(argent) then return end
-            SameZ = SameZ or false -- Should the Z of the pos be the same as the NPC's?
-            local myNearestPoint = self:GetOwner():EyePos()
-            local NearestPositions = {MyPosition=Vector(0,0,0), EnemyPosition=Vector(0,0,0)}
-            local Pos_Enemy, Pos_Self = argent:NearestPoint(myNearestPoint + argent:OBBCenter()), self:NearestPoint(argent:GetPos() + self:OBBCenter())
-            Pos_Enemy.z, Pos_Self.z = argent:GetPos().z, myNearestPoint.z
-            if SameZ == true then
-                Pos_Enemy = Vector(Pos_Enemy.x,Pos_Enemy.y,self:SetNearestPointToEntityPosition().z)
-                Pos_Self = Vector(Pos_Self.x,Pos_Self.y,self:SetNearestPointToEntityPosition().z)
-            end
-            NearestPositions.MyPosition = Pos_Self
-            NearestPositions.EnemyPosition = Pos_Enemy
-            return NearestPositions
-        end
 
         if (game.SinglePlayer() && CLIENT) then return end
 
         -- Refactored code to "shove" L4D2 NPCs 
 
-        if ((self:GetOwner():KeyDown(IN_USE) || self.Melee) && self:GetOwner():KeyPressed(IN_ATTACK)) then 
+        if ((self:GetOwner():KeyDown(IN_USE) || self.Melee) && self:GetOwner():KeyPressed(IN_ATTACK) || self:GetOwner():KeyPressed(IN_ALT1)) then 
             self:SetSafety(false)
             if (self:CanMelee()) then
-                print("goddamn i can melee now??")
                 if (CurTime() > self:GetNextMeleeTime()) then
 
                     self:SetIsReloading(false)
@@ -161,7 +156,7 @@ hook.Add( "Initialize", "ReplaceMeleeBehaviourModuleForVJL4D2", function()
                             dmgInfo:SetInflictor(self)
                             dmgInfo:SetAttacker(self:GetOwner())
                             dmgInfo:SetDamage(self.Animations["Melee_Hit"].Damage)
-                            dmgInfo:SetDamagePosition(GetNearestPoint(v).MyPosition)
+                            dmgInfo:SetDamagePosition(GetNearestPoint(self, v).MyPosition)
                             dmgInfo:SetDamageForce(self:GetOwner():EyeAngles():Forward() * (self.Animations["Melee_Hit"].Damage * (meleeRange)))
                             dmgInfo:SetDamageType(DMG_CLUB + DMG_ALWAYSGIB)
                             local inCone = (self:GetOwner():GetAimVector():Angle():Forward():Dot(((v:GetPos() +v:OBBCenter()) - self:GetOwner():GetShootPos()):GetNormalized()) > math.cos(math.rad((meleeRange))))
@@ -170,7 +165,7 @@ hook.Add( "Initialize", "ReplaceMeleeBehaviourModuleForVJL4D2", function()
                                     dmgInfo:SetDamage(self.Animations["Melee_Hit"].Damage * 0.5)
                                     bHitNPC = true
                                     bHit = true
-                                    Flinch(v)
+                                    Flinch(self,v)
                                 end 
                                 if hitCount < 3 then
                                     hitCount = hitCount + 1 
